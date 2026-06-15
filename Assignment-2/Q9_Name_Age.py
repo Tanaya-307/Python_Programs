6fdfde5e2e3e5513426eb8e37e972dd28b772364
Tanaya-307 <tanayaudaykar@gmail.com>
2026-06-15 14:50:00 +0530

    Assignment 2- Question 9
    Q9-Program to take user's name and age and display msg as :
    >Hello<name>,you will turn <age+1> next year.

---
+++ Assignment-2/Q9_Name_Age.py
@@ -0,0 +1,19 @@
+print("Enter your name : ")
+Name = input()
+
+print("Enter your age : ")
+Age = int(input())
+
+print("Hello",Name,"you will turn",Age+1,"next year")
+
+
+'''
+OUTPUT : 
+
+Enter your name
+Tanaya
+Enter your age
+20
+Hello Tanaya you will turn 21 next year
+
+'''
