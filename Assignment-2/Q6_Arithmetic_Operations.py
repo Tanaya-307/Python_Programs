5469043e5264c13b6e79eefe1df37063390d3e6a
Tanaya-307 <tanayaudaykar@gmail.com>
2026-06-15 14:48:04 +0530

    Assignment 2- Question 6
    Q6-Accept 2 numbers from user and perform :
    >Addition
    >Subtraction
    >Multiplication
    >Division

---
+++ Assignment-2/Q6_Arithmetic_Operations.py
@@ -0,0 +1,39 @@
+print("Enter 1st number : ")
+No1=int(input())
+
+print("Enter 2nd number : ")
+No2=int(input())
+
+print("---"*7)
+print("Arithmetic Operations")
+print("---"*7)
+
+Addition = No1 + No2 
+print("Addition = ",Addition)
+
+Subtraction = No1 - No2 
+print("Subtraction = ",Subtraction)
+
+Multiplication = No1 * No2
+print("Multiplication = ",Multiplication)
+
+Division = No1 / No2
+print("Division = ",Division)
+
+'''
+OUTPUT : 
+
+Enter 1st number :
+20
+Enter 2nd number :
+10
+---------------------
+Arithmetic Operations
+---------------------
+Addition =  30
+Subtraction =  10
+Multiplication =  200
+Division =  2.0
+
+
+'''
