b9213b508dfe5f025d7177262568076b860779ec
Tanaya-307 <tanayaudaykar@gmail.com>
2026-06-22 23:14:58 +0530

    Assignment_3Q6
    Q6-program to display:
    -Data type
    -Memory Address
    -Size in bytes

---
+++ Assignment_3/Q6_type_id_size.py
@@ -0,0 +1,17 @@
+from sys import getsizeof
+
+no=int(input("enter the number : "))
+print("Value : ",no)
+print("Data Type : ",type(no))
+print("Memory Addresss : ",id(no))
+print("Size in bytes : ",getsizeof(no))
+
+
+'''
+enter the number : 10
+Value :  10
+Data Type :  <class 'int'>
+Memory Addresss :  140728764122520
+Size in bytes :  28
+
+'''
